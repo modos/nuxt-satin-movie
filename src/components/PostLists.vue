f5c09648eeea747e1fd75ea8d6d2cf58938c5ebf
mohammad hossein mazandaranian <modos.m98@gmail.com>
2023-04-12 19:55:11 +0330

    1. component columns intitialized

--- src/components/PostLists.vue
+++ src/components/PostLists.vue
@@ -1,3 +1,33 @@
 <template>
-    posts list
+    <div class="posts-list-item flex flex-row-reverse">
+        <div class="posts-list-item__container flex flex-row p-2 basis-3/4">
+            <div class="posts-list-item__container__body flex flex-row-reverse justify-between w-full">
+                <p>
+                    دانلود فیلم Jesus Revolution 2023 انقلاب عیسی
+                </p>
+                <div class="posts-list-item__container__body__actions flex flex-row-reverse">
+                    <button type="button" class="fm-overlay-item addTo fm-post-10391"
+                        data-nonce="ab7464d254" data-toggle="tooltip" data-placement="top" title="افزودن به لیست تماشا"
+                        aria-label="Add To Watchlist"><span class="svg-icon ml-0 icon-bookmark"><svg
+                                xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-bookmark" viewBox="0 0 16 16">
+                                <path
+                                    d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z">
+                                </path>
+                            </svg></span> </button>
+                    <button class="addToNotify" data-pid="0" data-uid="0" data-logged="false" title="افزودن به اعلان ها">
+                        <span class="svg-icon ml-0 icon-bell"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+                            <path
+                                d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z">
+                            </path>
+                        </svg></span>
+                    </button>
+            </div>
+        </div>
+    </div>
+    <div class="basis-1/4">
+        thumbnail
+    </div>
+</div>
 </template>
