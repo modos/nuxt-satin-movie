e1908d345e59af608894551af0ec65370e6eb587
mohammad hossein mazandaranian <modos.m98@gmail.com>
2023-04-15 00:38:00 +0330

    1. body title rating added.

--- src/components/PostLists.vue
+++ src/components/PostLists.vue
@@ -2,40 +2,76 @@
     <div class="posts-list-item flex flex-col">
         <div class="flex flex-row-reverse justify-between p-2">
             <p>
-                    دانلود فیلم Jesus Revolution 2023 انقلاب عیسی
-                </p>
-                <div class="posts-list-item__container__body__actions flex flex-row-reverse">
-                    <button type="button" class="fm-overlay-item addTo fm-post-10391" data-nonce="ab7464d254"
-                        data-toggle="tooltip" data-placement="top" title="افزودن به لیست تماشا"
-                        aria-label="Add To Watchlist"><span class="svg-icon ml-0 icon-bookmark"><svg
-                                xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-bookmark" viewBox="0 0 16 16">
-                                <path
-                                    d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z">
-                                </path>
-                            </svg></span> </button>
-                    <button class="addToNotify" data-pid="0" data-uid="0" data-logged="false" title="افزودن به اعلان ها">
-                        <span class="svg-icon ml-0 icon-bell"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
-                                <path
-                                    d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z">
-                                </path>
-                            </svg></span>
-                    </button>
-                </div>
-        </div>
-        <div class="flex flex-row-reverse mt-5">
-            <div class="posts-list-item__container flex flex-row p-2 basis-3/4">
-            <div class="posts-list-item__container__body flex flex-row-reverse justify-between w-full">
-                body
+                دانلود فیلم Jesus Revolution 2023 انقلاب عیسی
+            </p>
+            <div class="posts-list-item__container__body__actions flex flex-row-reverse">
+                <button type="button" class="fm-overlay-item addTo fm-post-10391" data-nonce="ab7464d254"
+                    data-toggle="tooltip" data-placement="top" title="افزودن به لیست تماشا"
+                    aria-label="Add To Watchlist"><span class="svg-icon ml-0 icon-bookmark"><svg
+                            xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-bookmark" viewBox="0 0 16 16">
+                            <path
+                                d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z">
+                            </path>
+                        </svg></span> </button>
+                <button class="addToNotify" data-pid="0" data-uid="0" data-logged="false" title="افزودن به اعلان ها">
+                    <span class="svg-icon ml-0 icon-bell"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                            fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+                            <path
+                                d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z">
+                            </path>
+                        </svg></span>
+                </button>
             </div>
         </div>
-        <div class="posts-list-item__thumbnail basis-1/4 flex flex-col justify-center items-center">
-           <a>
-            <img src="https://filmsara.info/wp-content/uploads/2023/02/jesus-revolution-10391-210x315.jpg" width="210"
-                height="315">
-           </a>
-        </div>
-        </div>
+        <div class="flex flex-row-reverse mt-5">
+            <div class="posts-list-item__container flex flex-row px-2 basis-3/4">
+                <div class="posts-list-item__container__body flex flex-row-reverse justify-between w-full">
+                   <div class="posts-list-item__container__body__rating">
+                    <a class="flex flex-row items-center gap-1" style="direction: rtl;" rel="nofollow noreferrer">
+                        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="48px" height="48px"
+                                style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+                                xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <g>
+                                    <path style="opacity:0.988" fill="#f7bc0a"
+                                        d="M 47.5,13.5 C 47.5,20.8333 47.5,28.1667 47.5,35.5C 31.5,36.8333 15.5,36.8333 -0.5,35.5C -0.5,28.1667 -0.5,20.8333 -0.5,13.5C 15.5,12.1667 31.5,12.1667 47.5,13.5 Z" />
+                                </g>
+                                <g>
+                                    <path style="opacity:1" fill="#383c35"
+                                        d="M 7.5,17.5 C 8.5,17.5 9.5,17.5 10.5,17.5C 10.5,21.8333 10.5,26.1667 10.5,30.5C 9.5,30.5 8.5,30.5 7.5,30.5C 7.5,26.1667 7.5,21.8333 7.5,17.5 Z" />
+                                </g>
+                                <g>
+                                    <path style="opacity:1" fill="#3a3d35"
+                                        d="M 11.5,17.5 C 13.1667,17.5 14.8333,17.5 16.5,17.5C 16.8333,22.8333 17.1667,22.8333 17.5,17.5C 19.1667,17.5 20.8333,17.5 22.5,17.5C 22.5,21.8333 22.5,26.1667 22.5,30.5C 21.5,30.5 20.5,30.5 19.5,30.5C 19.4733,22.6064 18.8066,22.6064 17.5,30.5C 16.066,28.7801 15.2326,26.7801 15,24.5C 14.5045,26.4727 14.3379,28.4727 14.5,30.5C 13.5,30.5 12.5,30.5 11.5,30.5C 11.5,26.1667 11.5,21.8333 11.5,17.5 Z" />
+                                </g>
+                                <g>
+                                    <path style="opacity:1" fill="#353a36"
+                                        d="M 23.5,17.5 C 25.8568,17.3367 28.1902,17.5034 30.5,18C 31.8333,22 31.8333,26 30.5,30C 28.1902,30.4966 25.8568,30.6633 23.5,30.5C 23.5,26.1667 23.5,21.8333 23.5,17.5 Z" />
+                                </g>
+                                <g>
+                                    <path style="opacity:1" fill="#273337"
+                                        d="M 36.5,20.5 C 36.1866,23.2469 36.52,25.9135 37.5,28.5C 37.9201,25.3837 38.9201,22.7171 40.5,20.5C 40.5,23.8333 40.5,27.1667 40.5,30.5C 38.1667,30.5 35.8333,30.5 33.5,30.5C 33.5,26.1667 33.5,21.8333 33.5,17.5C 34.5,17.5 35.5,17.5 36.5,17.5C 36.5,18.5 36.5,19.5 36.5,20.5 Z" />
+                                </g>
+                                <g>
+                                    <path style="opacity:1" fill="#ba9318"
+                                        d="M 26.5,19.5 C 27.7647,20.5145 28.4313,22.0145 28.5,24C 28.1667,25.5 27.8333,27 27.5,28.5C 26.5159,25.5719 26.1826,22.5719 26.5,19.5 Z" />
+                                </g>
+                                <g>
+                                    <path style="opacity:1" fill="#665b2c"
+                                        d="M 40.5,20.5 C 38.9201,22.7171 37.9201,25.3837 37.5,28.5C 36.52,25.9135 36.1866,23.2469 36.5,20.5C 37.9613,19.3143 39.2946,19.3143 40.5,20.5 Z" />
+                                </g>
+                            </svg>
+                        <div><span>8.7</span> از 10</div>
+                        <div>میانگین رای 207 نفر</div>
+                    </a>
+                   </div>
+                </div>
+            </div>
+            <div class="posts-list-item__thumbnail basis-1/4 flex flex-col justify-center items-center">
+                <a>
+                    <img src="https://filmsara.info/wp-content/uploads/2023/02/jesus-revolution-10391-210x315.jpg"
+                        width="210" height="315">
+                </a>
+            </div>
     </div>
-</template>
+</div></template>
